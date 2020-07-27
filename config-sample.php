3c6893b0a2ca6866ae8797a6909b6ad77cdae654
fgilles <fgilles@ds-agency.com>
2020-07-27 16:06:37 +0200

    add files for sending or getting php bdd

---
+++ config-sample.php
@@ -0,0 +1,8 @@
+<?php
+$config = array();
+$config['get_string_to_allow'] = '';
+$config['host'] = '';
+$config['user'] = '';
+$config['pass'] = '';
+$config['bdd'] = '';
+$config['dump_file'] = '';
---
+++ getbdd.php
@@ -0,0 +1,11 @@
+<?php
+if (file_exists('config.php')) {
+    require_once('config.php');
+    if (isset($_GET['ddsbdd']) && $_GET['ddsbdd'] == $config['get_string_to_allow']) {
+            exec('/usr/bin/mysqldump -h ' . $config['host'] . ' -u ' . $config['user'] . ' -p\'' . $config['pass'] . '\' -B ' . $config['bdd'] . ' > ' . $config['dump_file'], $output);
+    } else {
+        die('not allowed');
+    }
+} else {
+    die('config.php not set');
+}
