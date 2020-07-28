f12bb5744fa0f87f1f7e46f3b9270c557f24737b
fgilles <fgilles@ds-agency.com>
2020-07-28 18:15:52 +0200

    add get stuff to empty db

--- getbdd.php
+++ getbdd.php
@@ -2,7 +2,10 @@
 if (file_exists('config.php')) {
     require_once('config.php');
     if (isset($_GET['ddsbdd']) && $_GET['ddsbdd'] == $config['get_string_to_allow']) {
-            exec('/usr/bin/mysqldump -h ' . $config['host'] . ' -u ' . $config['user'] . ' -p\'' . $config['pass'] . '\' -B ' . $config['bdd'] . ' > ' . $config['dump_file'], $output);
+        if (isset($_GET['get_empty']) && $_GET['get_empty'] == true) {
+            exec('/usr/bin/mysqldump --opt --no-data --no-create-db -h ' . $config['host'] . ' -u ' . $config['user'] . ' -p\'' . $config['pass'] . '\' -B ' . $config['bdd'] . ' > empty_' . $config['dump_file'], $output);
+        }
+        exec('/usr/bin/mysqldump -h ' . $config['host'] . ' -u ' . $config['user'] . ' -p\'' . $config['pass'] . '\' -B ' . $config['bdd'] . ' > ' . $config['dump_file'], $output);
     } else {
         die('not allowed');
     }
